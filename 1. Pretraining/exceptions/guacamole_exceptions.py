f2df27b7fafa9de3acbefa2acddd21d3f99d2f1a
Anton Morgunov <morgunov@mit.edu>
2023-08-06 11:57:43 -0400

    start watching dataset plots

---
+++ 1. Pretraining/exceptions/guacamole_exceptions.py
@@ -0,0 +1,52 @@
+ATOMS = {
+    "Ag",
+    "Al",
+    "Am",
+    "Ar",
+    "At",
+    "Au",
+    "D",
+    "E",
+    "Fe",
+    "G",
+    "K",
+    "L",
+    "M",
+    "Ra",
+    "Re",
+    "Rf",
+    "Rg",
+    "Rh",
+    "Ru",
+    "T",
+    "U",
+    "V",
+    "W",
+    "Xe",
+    "Y",
+    "Zr",
+    "a",
+    "d",
+    "f",
+    "g",
+    "h",
+    "k",
+    "m",
+    "si",
+    "t",
+    "te",
+    "u",
+    "v",
+    "y",
+}
+
+
+exceptions = set()
+for atom in ATOMS:
+    exceptions.add(atom)
+    exceptions.add(f"[{atom}]")
+import yaml
+
+# dump set into a yaml file
+with open("guacamole_exceptions.yaml", "w") as f:
+    yaml.dump(list(exceptions), f)
