5dcf356383caedd4930f3e1db243421f524b2367
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2023-09-28 13:40:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name = 'ChemSpaceAL',
     packages = ['ChemSpaceAL'],
-    version = '1.0.2',
+    version = '1.0.3',
     description = 'ChemSpaceAL Python package: an efficient active learning methodology applied to protein-specific molecular generation',
     install_requires=[
     'prolif==2.0.0.post1',
@@ -14,7 +14,7 @@ setup(
     author = 'Gregory W. Kyro, Anton Morgunov & Rafael I. Brent',
     author_email = 'gregory.kyro@yale.edu',
     url = 'https://github.com/gregory-kyro/ChemSpaceAL/tree/main',
-    download_url = 'https://github.com/gregory-kyro/ChemSpaceAL/archive/refs/tags/v1.0.2.tar.gz',
+    download_url = 'https://github.com/gregory-kyro/ChemSpaceAL/archive/refs/tags/v1.0.3.tar.gz',
     keywords = ['active learning', 'artificial intelligence', 'deep learning', 'machine learning', 'molecular generation', 'drug discovery'],
     classifiers = []
 )
