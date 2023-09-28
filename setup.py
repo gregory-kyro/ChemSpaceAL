e965457306c3585d944845c6bac99f781394a97e
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2023-09-28 11:54:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from distutils.core import setup
 
+
+def read_requirements():
+    with open('requirements.txt', 'r') as file:
+        return [line.strip() for line in file.readlines() if not line.startswith('#')]
+
+
 setup(
     name = 'ChemSpaceAL',
     packages = ['ChemSpaceAL'],
     version = '1.0.1',
     description = 'ChemSpaceAL Python package: an efficient active learning methodology applied to protein-specific molecular generation',
+    install_requires=read_requirements(),
     author = 'Gregory W. Kyro, Anton Morgunov & Rafael I. Brent',
     author_email = 'gregory.kyro@yale.edu',
     url = 'https://github.com/gregory-kyro/ChemSpaceAL/tree/main',
