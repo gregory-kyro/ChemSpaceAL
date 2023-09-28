5384dfb540cafcaba275a9fc4ccb31b097ff2620
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2023-09-28 09:54:51 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'ChemSpaceAL',
+    packages = ['ChemSpaceAL'],
+    version = '1.0.0',
+    description = 'ChemSpaceAL Python package: an efficient active learning methodology applied to protein-specific molecular generation',
+    author = 'Gregory W. Kyro',
+    author_email = 'gregory.kyro@yale.edu',
+    #CHANGE url = 'https://github.com/gregory-kyro/HAC-Net/tree/main/HACNet',
+    #CHANGE download_url = 'https://github.com/gregory-kyro/HAC-Net/archive/refs/tags/v1.4.3.tar.gz',
+    keywords = ['active learning', 'artificial intelligence', 'deep learning', 'machine learning', 'molecular generation', 'drug discovery'],
+    classifiers = []
+)
